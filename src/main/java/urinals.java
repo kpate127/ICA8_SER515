0f384e84ad939a9d48bc8b5b7d84e77f611839fd
Kunal Patel <kpate127@asu.edu>
2022-10-27 21:04:57 -0700

    Test THREE EXECUTED SUCCESSFULLY

--- src/main/java/urinals.java
+++ src/main/java/urinals.java
@@ -3,30 +3,134 @@
 * Asurite ID : kpate127
 * ASU ID : 1225326674
  */
-
+import java.util.*;
+import java.io.*;
 public class urinals {
 
-
-    public boolean goodString(String string) {
-        for (int i = 0; i < string.length() - 1; i++)
-        {
-            if (string.charAt(i) == '1' || string.charAt(i) == '0')
-            {
-                if (string.charAt(i) == '1' && string.charAt(i + 1) == '1')
-                {
+    public static boolean countAvailableUrinals(char[] input) {
+        for (int i = 0; i < input.length; i++) {
+            if (input[i] == '1') {
+                if (((i >= 1) && (input[i - 1] == '1')) || ((i < input.length - 1) && (input[i + 1] == '1')))
                     return false;
+            }
+        }
+
+        return true;
+    }
+
+    public static int backtrack(char[] input, int idx, int ans) {
+        if (idx == input.length)
+            return 0;
+        else {
+            for (int i = idx; i < input.length; i++) {
+                if (input[i] == '0') {
+                    input[i] = '1';
+                    if (countAvailableUrinals(input))
+                        return backtrack(input, idx + 1, ans + 1);
+                    input[i] = '0';
                 }
             }
-            else
-            {
+        }
+
+        return ans;
+    }
+
+    public static boolean goodString(String input) {
+        for (int i = 0; i < input.length() - 1; i++) {
+            if (input.charAt(i) == '1' || input.charAt(i) == '0') {
+                if (input.charAt(i) == '1' && input.charAt(i + 1) == '1') {
+                    return false;
+                }
+            } else {
                 return false;
             }
         }
         return true;
     }
 
-    public boolean emptyString (String string)
-    {
+
+    public static boolean emptyString(String string) {
         return string.length() == 0;
     }
+
+    public static String keyboardInput() throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println("Enter the string: ");
+        String input = br.readLine();
+//        if (goodString(input)) {
+//            char[] charArray = input.toCharArray();
+//            System.out.println("Number of free urinals: " + backtrack(charArray, 0, 0));
+//        } else {
+//            System.out.println("Number of free urinals: " + -1);
+//        }
+        return input;
+    }
+
+    public static void fileInput(String s) throws IllegalArgumentException, FileNotFoundException {
+
+
+            File file = new File(s);
+            if (!file.exists()){
+                throw new FileNotFoundException();
+            }
+            Scanner sc = new Scanner(file);
+            while (sc.hasNextLine()) {
+                String input = sc.nextLine();
+//                if (goodString(input)) {
+//                    char[] charArray = input.toCharArray();
+//                    System.out.println(input);
+//                    System.out.println("Number of free urinals: " + backtrack(charArray, 0, 0));
+//                } else {
+//                    System.out.println(input);
+//                    System.out.println("Number of free urinals: " + -1);
+//                }
+
+                if(input.equals("EOF"))break;
+            }
+            sc.close();
+    }
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println(" Select 1) Keyboard Input 2) File Input");
+        String str = br.readLine();
+
+        if (str.equals("1")) {
+            String input = keyboardInput();
+        } else if (str.equals("1")) {
+            String address = "src/resources/urinal.dat";
+            fileInput(address);
+        }
+    }
 }
+//            System.out.println("Enter the string: ");
+//            String input = br.readLine();
+//            if(goodString(input)) {
+//                char[] charArray = input.toCharArray();
+//                System.out.println("Number of free urinals: " + backtrack(charArray, 0, 0));
+//            } else{
+//                System.out.println("Number of free urinals: " + -1);
+//            }
+//        else if(str.equals("2")) {
+//            try {
+//                File file = new File("src/resources/urinal.dat");
+//                Scanner sc = new Scanner(file);
+//                while (sc.hasNextLine()) {
+//                    String input = sc.nextLine();
+//                    if(goodString(input)) {
+//                        char[] charArray = input.toCharArray();
+//                        System.out.println(input);
+//                        System.out.println("Number of free urinals: " + backtrack(charArray, 0, 0));
+//                    } else{
+//                        System.out.println(input);
+//                        System.out.println("Number of free urinals: " + -1);
+//                    }
+//                }
+//                sc.close();
+//            } catch (FileNotFoundException e) {
+//                System.out.println("An error occurred.");
+//                e.printStackTrace();
+//            }
+
--- src/test/java/UrinalTest.java
+++ src/test/java/UrinalTest.java
@@ -1,5 +1,7 @@
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
+import java.io.*;
+
 
 public class UrinalTest {
 
@@ -16,6 +18,24 @@ public class UrinalTest {
         urinals U = new urinals();
         assertEquals(false, U.emptyString(str2));
         System.out.println("====== KUNAL JITENDRA PATEL == TEST TWO EXECUTED =======");
+    }
+
+    @Test
+    public void checkCountOfAvailableUrinalsTest(){
+        String str1 = "10001";
+        String str2 = "1001";
+        String str3 = "00000";
+        String str4 = "0000";
+        String str5 = "01000";
+        String str6 = "011";
+        urinals U = new urinals();
+        assertEquals(1, U.countAvailableUrinals(str1.toCharArray()));
+        assertEquals(0, U.countAvailableUrinals(str2.toCharArray()));
+        assertEquals(3, U.countAvailableUrinals(str3.toCharArray()));
+        assertEquals(2, U.countAvailableUrinals(str4.toCharArray()));
+        assertEquals(1, U.countAvailableUrinals(str5.toCharArray()));
+        assertEquals(-1, U.countAvailableUrinals(str6.toCharArray()));
+        System.out.println("====== KUNAL JITENDRA PATEL == TEST THREE EXECUTED =======");
     }
 
 
